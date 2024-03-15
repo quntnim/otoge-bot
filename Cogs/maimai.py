5a560dc1615d65d39b4af9b274e579437a2dd1db
quntnim <genshin_imp@dgsw.hs.kr>
2024-03-15 15:49:05 +0900

    봇 리로드 기능 추가 및 maimai 데이터 추가

--- Cogs/maimai.py
+++ Cogs/maimai.py
@@ -1,3 +1,6 @@
+import datetime
+import json
+
 import discord
 from discord.ext import commands
 
@@ -9,6 +12,42 @@ class Maimai(commands.Cog):
     @commands.command()
     async def maimai(self, ctx):
         await ctx.send("maimai 명령어 테스트")
+
+    @commands.command(name='mai-search')
+    async def search(self, ctx, arg):
+        with open('./data/maimai_data.json', 'r', encoding='UTF-8') as f:
+            json_data = json.load(f)
+            for k in json_data['songs']:
+                if arg.upper() in k['songId'].upper():
+                    print(k)
+
+        embed = discord.Embed(
+            title="Straight into the lights\n<:dx1:1218056379458261112><:dx2:1218056391982583849><:dx3:1218056405681180813>",
+            url="https://www.youtube.com/results?search_query=maimai+Straight+into+the+lights",
+            description="> Cosmograph / maimai",
+            colour=0x00b0f4,
+            timestamp=datetime.datetime.now())
+
+        embed.set_author(name="곡 검색", url="https://example.com")
+
+        embed.add_field(name="BASIC",
+                        value="4",
+                        inline=False)
+        embed.add_field(name="ADVANCED",
+                        value="8",
+                        inline=False)
+        embed.add_field(name="EXPERT",
+                        value="12 (12.5)",
+                        inline=False)
+        embed.add_field(name="MASTER",
+                        value="14+ (14.8)",
+                        inline=False)
+        embed.set_thumbnail(
+            url="https://dp4p6x0xfi5o9.cloudfront.net/maimai/img/cover/8fc940a2fcffd56224e372b2a57888e84ca295c43609dad8a185b14227caf2a8.png")
+
+        embed.set_footer(text="otoge-bot")
+
+        await ctx.send(embed=embed)
 
 
 async def setup(app):
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import load_json_variable as variable
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=".", intents=intents)
+cogs_path = 'Cogs'
+abs_cogs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), cogs_path)
 
 
 @bot.event
@@ -31,12 +33,51 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
+async def unload_extensions(extension=None):
+    if extension is not None:
+        try:
+            await bot.unload_extension(f"Cogs.{extension}")
+        except (commands.ExtensionNotLoaded, commands.ExtensionNotFound):
+            pass
+    else:
+        for filename in os.listdir("Cogs"):
+            if filename.endswith('.py'):
+                try:
+                    await bot.unload_extension(f"Cogs.{filename[:-3]}")
+                except (commands.ExtensionNotLoaded, commands.ExtensionNotFound):
+                    pass
+
+
 async def load_extensions():
-    cogs_path = 'Cogs'
-    abs_cogs_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), cogs_path)
     for filename in os.listdir(abs_cogs_path):
         if filename.endswith('.py'):
             await bot.load_extension(f'Cogs.{filename[:-3]}')
+
+
+@bot.command(name='reload')
+async def reload_extensions(ctx, extension=None):
+    if extension is not None:
+        await unload_extensions(extension)
+        try:
+            await bot.load_extension(f"Cogs.{extension}")
+        except commands.ExtensionNotFound:
+            await ctx.send(f"'{extension}' 파일을 찾을 수 없습니다.")
+        except (commands.NoEntryPointError, commands.ExtensionFailed) as e:
+            await ctx.send(f"'{extension}' 을(를) 불러오는 도중 에러가 발생했습니다.\n{e}")
+        else:
+            await ctx.send(":white_check_mark:")
+    else:
+        for filename in os.listdir(abs_cogs_path):
+            if filename.endswith('.py'):
+                await unload_extensions(filename[:-3])
+                try:
+                    await bot.load_extension(f"Cogs.{filename[:-3]}")
+                except commands.ExtensionNotFound:
+                    await ctx.send(f"'{filename[:-3]}' 파일을 찾을 수 없습니다.")
+                except (commands.NoEntryPointError, commands.ExtensionFailed) as e:
+                    await ctx.send(f"'{filename[:-3]}' 을(를) 불러오는 도중 에러가 발생했습니다.\n{e}")
+
+        await ctx.send(":white_check_mark:")
 
 
 async def main():
